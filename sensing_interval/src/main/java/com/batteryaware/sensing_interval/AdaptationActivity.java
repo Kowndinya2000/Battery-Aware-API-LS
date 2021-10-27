cf30a40fafce094685a5e9b18f3ea0ffe2dc5a5a
Kowndinya Boyalakuntla <46968076+Kowndinya2000@users.noreply.github.com>
2021-10-27 12:03:28 +0530

    Update MainActivity

--- sensing_interval/src/main/java/com/batteryaware/sensing_interval/AdaptationActivity.java
+++ sensing_interval/src/main/java/com/batteryaware/sensing_interval/AdaptationActivity.java
@@ -6,9 +6,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 import java.util.concurrent.TimeUnit;
 
@@ -81,7 +83,6 @@ public class AdaptationActivity extends Activity {
     private AdaptiveEngine setUpPolicy_HDB = new AdaptiveEngine();
     private void setSetUpPolicy_HDB(AdaptiveEngine setUpPolicy_HDB) {
         this.setUpPolicy_HDB = setUpPolicy_HDB;
-//        this.setUpPolicy_HDB.SENSING_INTERVAL = setUpPolicy_HCB.SENSING_INTERVAL + ((int) Math.exp(Math.sqrt(setUpPolicy_HCB.SLOPE))*100*(100-defineLevel.BATTERY_HIGH));
         this.setUpPolicy_HDB.SENSING_INTERVAL = 1000;
         this.setUpPolicy_HDB.SLOPE = 2;
         this.setUpPolicy_HDB.BatteryAwareFunction = "Exponential";
@@ -96,7 +97,6 @@ public class AdaptationActivity extends Activity {
     private AdaptiveEngine setUpPolicy_MCF = new AdaptiveEngine();
     private void setSetUpPolicy_MCF(AdaptiveEngine setUpPolicy_MCF) {
         this.setUpPolicy_MCF = setUpPolicy_MCF;
-        //  this.setUpPolicy_MCF.SENSING_INTERVAL = this.setUpPolicy_HDB.SENSING_INTERVAL = setUpPolicy_HCB.SENSING_INTERVAL + ((int) Math.exp(Math.sqrt(setUpPolicy_HCB.SLOPE))*100*(100-defineLevel.BATTERY_HIGH));;
         this.setUpPolicy_MCF.SENSING_INTERVAL = 1000;
         this.setUpPolicy_MCF.SLOPE = 0;
         this.setUpPolicy_MCF.BatteryAwareFunction = "Linear";
@@ -125,7 +125,7 @@ public class AdaptationActivity extends Activity {
     private AdaptiveEngine setUpPolicy_MDF = new AdaptiveEngine();
     private void setSetUpPolicy_MDF(AdaptiveEngine setUpPolicy_MDF) {
         this.setUpPolicy_MDF = setUpPolicy_MDF;
-        this.setUpPolicy_MDF.SENSING_INTERVAL = setUpPolicy_HDF.SENSING_INTERVAL + (setUpPolicy_HDF.SLOPE*100*(100-defineLevel.BATTERY_HIGH));
+        this.setUpPolicy_MDF.SENSING_INTERVAL = setUpPolicy_HDF.SENSING_INTERVAL + setUpPolicy_HDF.SLOPE*(100-defineLevel.BATTERY_HIGH);
         this.setUpPolicy_MDF.SLOPE = 2;
         this.setUpPolicy_MDF.BatteryAwareFunction = "Linear";
     }
@@ -181,7 +181,7 @@ public class AdaptationActivity extends Activity {
     private AdaptiveEngine setUpPolicy_LDF = new AdaptiveEngine();
     private void setSetUpPolicy_LDF(AdaptiveEngine setUpPolicy_LDF) {
         this.setUpPolicy_LDF = setUpPolicy_LDF;
-        this.setUpPolicy_LDF.SENSING_INTERVAL = setUpPolicy_MDF.SENSING_INTERVAL + (setUpPolicy_MDF.SLOPE*100*(100-defineLevel.BATTERY_LOW));
+        this.setUpPolicy_LDF.SENSING_INTERVAL = setUpPolicy_MDF.SENSING_INTERVAL + setUpPolicy_MDF.SLOPE*(100-defineLevel.BATTERY_LOW);
         this.setUpPolicy_LDF.SLOPE = 2;
         this.setUpPolicy_LDF.BatteryAwareFunction = "Exponential";
     }
@@ -207,171 +207,121 @@ public class AdaptationActivity extends Activity {
     }
 
     private int level;
-    private Boolean returned = false;
     private boolean isCharging;
     private boolean appInForeground;
-    private BroadcastReceiver InformationReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-            int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-            isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL;
-            levelReturned();
-        }
-    };
-    private void levelReturned(){
-        returned = true;
-    }
-    protected int returnLevel() {
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    protected int returnLevel(Context context) {
         int BATTERY_AWARE_SI = 1000;
-        returned = false;
-        this.registerReceiver(this.InformationReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-        try {
-            TimeUnit.MILLISECONDS.sleep(500);
-        }
-        catch(Exception e){
-            System.out.println("Fallen into exception");
-        }
-        if (appInForeground)
-        {
-            if(level >= defineLevel.BATTERY_HIGH)
-            {
-                if(isCharging)
-                {
-                    if(setUpPolicy_HCF.BatteryAwareFunction.equals("Linear"))
-                    {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_HCF.SENSING_INTERVAL + (setUpPolicy_HCF.SLOPE*100*(100-level)));
-                    }
-                    else {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_HCF.SENSING_INTERVAL + (Math.exp(Math.sqrt(setUpPolicy_HCF.SLOPE))*100*(100-level)));
-                    }
-                }
-                else {
-                    if(setUpPolicy_HDF.BatteryAwareFunction.equals("Linear"))
-                    {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_HDF.SENSING_INTERVAL + (setUpPolicy_HDF.SLOPE*100*(100-level)));
-                    }
-                    else {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_HDF.SENSING_INTERVAL + (Math.exp(Math.sqrt(setUpPolicy_HDF.SLOPE))*100*(100-level)));
-                    }
-                }
+        BatteryManager bm = (BatteryManager) context.getSystemService(BATTERY_SERVICE);
+        if(bm != null) {
+            level = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+            int status = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_STATUS);
+            switch (status) {
+                case BatteryManager.BATTERY_STATUS_CHARGING:
+                    isCharging = true;
+                    break;
+                case BatteryManager.BATTERY_STATUS_DISCHARGING:
+                    isCharging = false;
+                    break;
             }
-            else if(level >= defineLevel.BATTERY_MEDIUM)
-            {
-                if(isCharging)
-                {
-                    if(setUpPolicy_MCF.BatteryAwareFunction.equals("Linear"))
-                    {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_MCF.SENSING_INTERVAL + (setUpPolicy_MCF.SLOPE*100*(100-level)));
+            if (appInForeground) {
+                if (level > defineLevel.BATTERY_HIGH) {
+                    if (isCharging) {
+                        if (setUpPolicy_HCF.BatteryAwareFunction.equals("Linear")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_HCF.SENSING_INTERVAL + setUpPolicy_HCF.SLOPE * (100 - level));
+                        } else if (setUpPolicy_HCF.BatteryAwareFunction.equals("Exponential")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_HCF.SENSING_INTERVAL + (Math.exp(setUpPolicy_HCF.SLOPE/(float)7)) * (100 - level));
+                        }
+                    } else {
+                        if (setUpPolicy_HDF.BatteryAwareFunction.equals("Linear")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_HDF.SENSING_INTERVAL + setUpPolicy_HDF.SLOPE * (100 - level));
+                        } else if (setUpPolicy_HDF.BatteryAwareFunction.equals("Exponential"))  {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_HCF.SENSING_INTERVAL + (Math.exp(setUpPolicy_HDF.SLOPE/(float)7)) * (100 - level));
+                        }
                     }
-                    else {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_MCF.SENSING_INTERVAL + (Math.exp(Math.sqrt(setUpPolicy_MCF.SLOPE))*100*(100-level)));
+                } else if (level > defineLevel.BATTERY_MEDIUM) {
+                    if (isCharging) {
+                        if (setUpPolicy_MCF.BatteryAwareFunction.equals("Linear")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_MCF.SENSING_INTERVAL + setUpPolicy_MCF.SLOPE * (100 - level));
+                        } else if (setUpPolicy_MCF.BatteryAwareFunction.equals("Exponential")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_HCF.SENSING_INTERVAL + (Math.exp(setUpPolicy_MCF.SLOPE/(float)7)) * (100 - level));
+                        }
+                    } else {
+                        if (setUpPolicy_MDF.BatteryAwareFunction.equals("Linear")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_MDF.SENSING_INTERVAL + setUpPolicy_MDF.SLOPE * (100 - level));
+                        } else if (setUpPolicy_MDF.BatteryAwareFunction.equals("Exponential")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_HCF.SENSING_INTERVAL + (Math.exp(setUpPolicy_MDF.SLOPE/(float)7)) * (100 - level));
+
+                        }
                     }
-                }
-                else {
-                    if(setUpPolicy_MDF.BatteryAwareFunction.equals("Linear"))
-                    {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_MDF.SENSING_INTERVAL + (setUpPolicy_MDF.SLOPE*100*(100-level)));
-                    }
-                    else {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_MDF.SENSING_INTERVAL + (Math.exp(Math.sqrt(setUpPolicy_MDF.SLOPE))*100*(100-level)));
-                    }
-                }
-            }
-            else
-            {
-                if(isCharging)
-                {
-                    if(setUpPolicy_LCF.BatteryAwareFunction.equals("Linear"))
-                    {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_LCF.SENSING_INTERVAL + (setUpPolicy_LCF.SLOPE*100*(100-level)));
-                    }
-                    else {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_LCF.SENSING_INTERVAL + (Math.exp(Math.sqrt(setUpPolicy_LCF.SLOPE))*100*(100-level)));
-                    }
-                }
-                else {
-                    if(setUpPolicy_LDF.BatteryAwareFunction.equals("Linear"))
-                    {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_LDF.SENSING_INTERVAL + (setUpPolicy_LDF.SLOPE*100*(100-level)));
-                    }
-                    else {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_LDF.SENSING_INTERVAL + (Math.exp(Math.sqrt(setUpPolicy_LDF.SLOPE))*100*(100-level)));
-                    }
-                }
-            }
-        }
-        else {
-            if(level >= defineLevel.BATTERY_HIGH)
-            {
-                if(isCharging)
-                {
-                    if(setUpPolicy_HCB.BatteryAwareFunction.equals("Linear"))
-                    {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_HCB.SENSING_INTERVAL + (setUpPolicy_HCB.SLOPE*100*(100-level)));
-                    }
-                    else {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_HCB.SENSING_INTERVAL + (Math.exp(Math.sqrt(setUpPolicy_HCB.SLOPE))*100*(100-level)));
+                } else {
+                    if (isCharging) {
+                        if (setUpPolicy_LCF.BatteryAwareFunction.equals("Linear")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_LCF.SENSING_INTERVAL + setUpPolicy_LCF.SLOPE * (100 - level));
+                        } else if (setUpPolicy_LCF.BatteryAwareFunction.equals("Exponential")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_HCF.SENSING_INTERVAL + (Math.exp(setUpPolicy_LCF.SLOPE/(float)7)) * (100 - level));
+
+                        }
+                    } else {
+                        if (setUpPolicy_LDF.BatteryAwareFunction.equals("Linear")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_LDF.SENSING_INTERVAL + setUpPolicy_LDF.SLOPE * (100 - level));
+                        } else if (setUpPolicy_LDF.BatteryAwareFunction.equals("Exponential")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_HCF.SENSING_INTERVAL + (Math.exp(setUpPolicy_LDF.SLOPE/(float)7)) * (100 - level));
+
+                        }
                     }
                 }
-                else {
-                    if(setUpPolicy_HDB.BatteryAwareFunction.equals("Linear"))
-                    {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_HDB.SENSING_INTERVAL + (setUpPolicy_HDB.SLOPE*100*(100-level)));
+            } else {
+                if (level > defineLevel.BATTERY_HIGH) {
+                    if (isCharging) {
+                        if (setUpPolicy_HCB.BatteryAwareFunction.equals("Linear")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_HCB.SENSING_INTERVAL + setUpPolicy_HCB.SLOPE * (100 - level));
+                        } else if (setUpPolicy_HCB.BatteryAwareFunction.equals("Exponential")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_HCF.SENSING_INTERVAL + (Math.exp(setUpPolicy_HCB.SLOPE/(float)7)) * (100 - level));
+
+                        }
+                    } else {
+                        if (setUpPolicy_HDB.BatteryAwareFunction.equals("Linear")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_HDB.SENSING_INTERVAL + setUpPolicy_HDB.SLOPE * (100 - level));
+                        } else if (setUpPolicy_HDB.BatteryAwareFunction.equals("Exponential")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_HCF.SENSING_INTERVAL + (Math.exp(setUpPolicy_HDB.SLOPE/(float)7)) * (100 - level));
+
+                        }
                     }
-                    else {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_HDB.SENSING_INTERVAL + (Math.exp(Math.sqrt(setUpPolicy_HDB.SLOPE))*100*(100-level)));
+                } else if (level > defineLevel.BATTERY_MEDIUM) {
+                    if (isCharging) {
+                        if (setUpPolicy_MCB.BatteryAwareFunction.equals("Linear")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_MCB.SENSING_INTERVAL + setUpPolicy_MCB.SLOPE * (100 - level));
+                        } else if (setUpPolicy_MCB.BatteryAwareFunction.equals("Exponential")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_HCF.SENSING_INTERVAL + (Math.exp(setUpPolicy_MCB.SLOPE/(float)7)) * (100 - level));
+                        }
+                    } else {
+                        if (setUpPolicy_MDB.BatteryAwareFunction.equals("Linear")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_MDB.SENSING_INTERVAL + setUpPolicy_MDB.SLOPE * (100 - level));
+                        } else if (setUpPolicy_MDB.BatteryAwareFunction.equals("Exponential")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_HCF.SENSING_INTERVAL + (Math.exp(setUpPolicy_MDB.SLOPE/(float)7)) * (100 - level));
+
+                        }
                     }
-                }
-            }
-            else if(level >= defineLevel.BATTERY_MEDIUM)
-            {
-                if(isCharging)
-                {
-                    if(setUpPolicy_MCB.BatteryAwareFunction.equals("Linear"))
-                    {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_MCB.SENSING_INTERVAL + (setUpPolicy_MCB.SLOPE*100*(100-level)));
-                    }
-                    else {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_MCB.SENSING_INTERVAL + (Math.exp(Math.sqrt(setUpPolicy_MCB.SLOPE))*100*(100-level)));
-                    }
-                }
-                else {
-                    if(setUpPolicy_MDB.BatteryAwareFunction.equals("Linear"))
-                    {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_MDB.SENSING_INTERVAL + (setUpPolicy_MDB.SLOPE*100*(100-level)));
-                    }
-                    else {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_MDB.SENSING_INTERVAL + (Math.exp(Math.sqrt(setUpPolicy_MDB.SLOPE))*100*(100-level)));
-                    }
-                }
-            }
-            else
-            {
-                if(isCharging)
-                {
-                    if(setUpPolicy_LCB.BatteryAwareFunction.equals("Linear"))
-                    {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_LCB.SENSING_INTERVAL + (setUpPolicy_LCB.SLOPE*100*(100-level)));
-                    }
-                    else {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_LCB.SENSING_INTERVAL + (Math.exp(Math.sqrt(setUpPolicy_LCB.SLOPE))*100*(100-level)));
-                    }
-                }
-                else {
-                    if(setUpPolicy_LDB.BatteryAwareFunction.equals("Linear"))
-                    {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_LDB.SENSING_INTERVAL + (setUpPolicy_LDB.SLOPE*100*(100-level)));
-                    }
-                    else {
-                        BATTERY_AWARE_SI = (int) (setUpPolicy_LDB.SENSING_INTERVAL + (Math.exp(Math.sqrt(setUpPolicy_LDB.SLOPE))*100*(100-level)));
+                } else {
+                    if (isCharging) {
+                        if (setUpPolicy_LCB.BatteryAwareFunction.equals("Linear")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_LCB.SENSING_INTERVAL + setUpPolicy_LCB.SLOPE * (100 - level));
+                        } else if (setUpPolicy_LCB.BatteryAwareFunction.equals("Exponential")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_HCF.SENSING_INTERVAL + (Math.exp(setUpPolicy_LCB.SLOPE/(float)7)) * (100 - level));
+                        }
+                    } else {
+                        if (setUpPolicy_LDB.BatteryAwareFunction.equals("Linear")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_LDB.SENSING_INTERVAL + setUpPolicy_LDB.SLOPE * (100 - level));
+                        } else if (setUpPolicy_LDB.BatteryAwareFunction.equals("Exponential")) {
+                            BATTERY_AWARE_SI = (int) (setUpPolicy_HCF.SENSING_INTERVAL + (Math.exp(setUpPolicy_LDB.SLOPE/(float)7)) * (100 - level));
+                        }
                     }
                 }
             }
         }
         return BATTERY_AWARE_SI;
     }
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -391,7 +341,6 @@ public class AdaptationActivity extends Activity {
         setSetUpPolicy_LDF(setUpPolicy_LDF);
         setSetUpPolicy_LDB(setUpPolicy_LDB);
         appInForeground = true;
-        this.registerReceiver(this.InformationReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
 
     @Override
